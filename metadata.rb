dc32a996019ecb7643d84c33e0c68f9cf919c58b
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-01-28 16:44:33 +0000

    Bumped version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'help@sous-chefs.org'
 license           'Apache 2.0'
 description       'Installs and configures DHCP'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '5.4.0'
+version           '5.4.1'
 
 source_url "https://github.com/sous-chefs/#{name}" if respond_to?(:source_url)
 issues_url "https://github.com/sous-chefs/#{name}/issues" if respond_to?(:issues_url)
